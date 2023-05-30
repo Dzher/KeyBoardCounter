489e192129202eb81493f455d9705d82669bad6d
SmithD11 <2539925009@qq.com>
2023-05-30 09:07:07 +0800

    add clang-format file, and change mainwindow to tabwidget

--- keyboardCounterUI.h
+++ keyboardCounterUI.h
@@ -1,2 +1,19 @@
-#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QTabWidget>
 #include <iostream>
+
+#ifndef _KEYBOARDCOUNTERUI_H_
+#    define _KEYBOARDCOUNTERUI_H_
+namespace kbcUI {
+class KeyboardCounterUI : public QTabWidget
+{
+    Q_OBJECT
+public:
+    KeyboardCounterUI(const QString& name, QWidget* parent = nullptr)
+        : QTabWidget(parent)
+    {}
+
+private:
+};
+}   // namespace kbcUI
+
+#endif   //_KEYBOARDCOUNTERUI_H_
